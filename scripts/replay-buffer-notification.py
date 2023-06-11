928b6f78602fb9e85e3389cd67881c1d0b0037ec
Juan Carlos Martín <55212637+Linkeriyo@users.noreply.github.com>
2023-06-11 02:34:12 +0200

    Completed opening explorer on replay buffer saved

--- scripts/replay-buffer-notification.py
+++ scripts/replay-buffer-notification.py
@@ -1,48 +1,38 @@
-import os
 from windows_toasts import WindowsToaster, ToastText1
 import obspython
 import simpleobsws
 import asyncio
 from showinfm import show_in_file_manager
 
-def show_notification(body, path = None):
+def show_notification(message, path=None):
     toaster = WindowsToaster('OBS Replay Buffer')
-
-    newToast = ToastText1()
-    newToast.SetBody(body)
+    newToast = ToastText1(body=message)
 
     if path is not None:
-        #TODO: this still doesn't work because the script is no longer running when the notification is clicked
         newToast.on_activated = lambda _: show_in_file_manager(path)
     
     toaster.show_toast(newToast)
 
 
 def on_event(event):
-    if event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING:
-        message = "Replay buffer starting"
-        print(message)
+    messages = {
+        obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING: "Replay buffer starting",
+        obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED: "Replay buffer started",
+        obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPING: "Replay buffer stopping",
+        obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED: "Replay buffer stopped",
+        obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED: "Replay buffer saved",
+    }
 
-    elif event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED:
-        message = "Replay buffer started"
-        print(message)
-        show_notification(message)
-
-    elif event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPING:
-        message = "Replay buffer stopping"
-        print(message)
-        show_notification(message)
-
-    elif event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED:
-        message = "Replay buffer stopped"
-        print(message)
+    message = messages.get(event)
     
-    elif event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
-        import debugpy;debugpy.breakpoint()
-        replay_path = async_to_sync(get_last_replay_buffer_path()) 
-        message = "Replay buffer saved"
-        print(message)
+    if event == obspython.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
+        replay_path = asyncio.run(get_last_replay_buffer_path())
+        show_notification(message, replay_path)
+    else:
         show_notification(message)
+
+    if message:
+        print(message)  
 
 
 async def get_last_replay_buffer_path():
@@ -54,20 +44,12 @@ async def get_last_replay_buffer_path():
     await client.disconnect()
 
     if result.ok():
-        print("Request succeeded! Response data: {}".format(result.responseData))
-        return result.responseData['savedReplayPath']
+        print(f"Request succeeded! Response data: {result.responseData}")
+        return result.responseData.get('savedReplayPath')
     else:
-        print("Request failed! Error: {}".format(result.error))
+        print(f"Request failed! Error: {result.error}")
         return None
-    
-
-def async_to_sync(coro):
-    return asyncio.get_event_loop().run_until_complete(coro)
     
 
 def script_load(settings):
     obspython.obs_frontend_add_event_callback(on_event)
-
-
-def script_description():
-    "Triggered when the current scene has changed."
